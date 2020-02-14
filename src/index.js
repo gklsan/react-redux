1a3ae3945e3d5ff067bb58ee070c598af50aed0c
Gokul <pgokulmca@gmail.com>
2020-02-14 23:50:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Entrypoint = _ => <h1>Welcome</h1>
+
+ReactDOM.render(<Entrypoint />, document.getElementById('root'));
